a696350c1c49593cfb8d1a11a55ffcd9d2e16510
brocchirodrigo <rodrigo.brocchi@gmail.com>
2021-03-14 14:41:36 -0300

    Validação para cpf existente

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.post('/account', (request, response) => {
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
   if (customerAlreadyExists) {
-    return response.status(400).json({ error: 'Customer already exists' })
+    return response.status(400).json({ error: 'Costumer already exists' })
   }
 
   customers.push({
@@ -34,6 +34,20 @@ app.post('/account', (request, response) => {
 
   return response.status(201).send();
 
-})
+});
+
+app.get('/statement', (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Cold not find the customer' })
+  }
+
+  return response.json(customer.statement);
+
+
+});
 
 app.listen(3333);
