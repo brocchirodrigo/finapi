f7c74e2cd7fcf5831576a071f0409f60a53bb369
brocchirodrigo <rodrigo.brocchi@gmail.com>
2021-03-14 17:21:49 -0300

    Extrato por data

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-
-const customers = [];
 /**
  * cpf - string
  * name - string
@@ -12,42 +10,135 @@ const customers = [];
  * statement []
  */
 
+const costumers = [];
+
+// Middleware
+
 app.use(express.json());
+
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  
+  const costumer = costumers.find(costumer => costumer.cpf === cpf);
+
+  if (!costumer) {
+    return response.status(400).json({ error: 'Cold not find the costumer' })
+  }
+
+  request.costumer = costumer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const { credit, debit } = statement.reduce((accumulated, operation) => {
+    switch (operation.type) {
+      case 'credit':
+        accumulated.credit += operation.amount
+        break
+      case 'debit':
+        accumulated.debit += operation.amount
+      default:
+        break
+    }
+    
+    return accumulated
+      
+    }, 
+    {
+      credit: 0,
+      debit: 0,
+      total: 0
+    }
+  )
+
+  const total = credit - debit
+
+  return { credit, debit, total };
+}
 
 app.post('/account', (request, response) => {
   const { name, cpf } = request.body;
 
-  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+  const costumerAlreadyExists = costumers.some((costumer) => costumer.cpf === cpf);
 
-  if (customerAlreadyExists) {
+  if (costumerAlreadyExists) {
     return response.status(400).json({ error: 'Costumer already exists' })
   }
 
-  customers.push({
+  costumers.push({
     cpf,
     name,
     id: uuidv4(),
     statement: [],
   })
 
-  console.log(customers)
-
   return response.status(201).send();
 
 });
 
+app.use(verifyIfExistsAccountCPF);
+
 app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
+  const { costumer } = request;
 
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Cold not find the customer' })
-  }
-
-  return response.json(customer.statement);
-
-
+  const balance = getBalance(costumer.statement); 
+  
+  return response.json({ statement: costumer.statement, balance}) 
 });
+
+app.post('/deposit', (request, response) => {
+  const { costumer } = request;
+  const { description, amount } = request.body;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  };
+
+  costumer.statement.push(statementOperation);
+
+  return response.status(201).json({ success: 'Deposit confirme in the date' })
+})
+
+app.post('/withdraw', (request, response) => {
+  const { costumer } = request;
+  const { description, amount } = request.body;
+
+  const balance = getBalance(costumer.statement);
+
+  if(balance.total < amount) {
+    return response.status(400).json({ erro: 'Insufficient funds!' })
+  }
+  
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  costumer.statement.push(statementOperation);
+
+  return response.status(201).json({ success: 'Withdraw confirme in the date' })
+})
+
+app.get('/statement/date', (request, response) => {
+  const { costumer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + ' 00:00');
+
+  const statement = costumer.statement.filter(
+    (statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  const balance = getBalance(statement); 
+  
+  return response.json({ dateStatement: date, statement, balance }) 
+});
+
 
 app.listen(3333);
