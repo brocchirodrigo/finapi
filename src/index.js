9eb4d8564087fbd05fb0c859ea1822fb8fbfd275
brocchirodrigo <rodrigo.brocchi@gmail.com>
2021-03-13 22:38:48 -0300

    Cadastro e checagem de CPF

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 
-const costumers = [];
+const customers = [];
 /**
  * cpf - string
  * name - string
@@ -16,16 +16,21 @@ app.use(express.json());
 
 app.post('/account', (request, response) => {
   const { name, cpf } = request.body;
-  const id = uuidv4();
 
-  costumers.push({
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists' })
+  }
+
+  customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   })
 
-  console.log(costumers)
+  console.log(customers)
 
   return response.status(201).send();
 
